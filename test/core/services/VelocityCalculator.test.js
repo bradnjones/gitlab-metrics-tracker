9c1cd881a7e4d8f4576974af88010bcc38ea72d1
Brad Jones <brad.jones@samaritanministries.org>
2025-11-07 04:51:28 -0600

    test: add VelocityCalculator test 1.2 - ignore open issues (#5)
    Validates that only closed issues are counted in velocity

--- test/core/services/VelocityCalculator.test.js
+++ test/core/services/VelocityCalculator.test.js
@@ -26,5 +26,29 @@ describe('VelocityCalculator', () => {
 
       expect(velocity).toBe(10); // 3 + 5 + 2
     });
+
+    it('should ignore open issues when calculating velocity', () => {
+      const issues = [
+        {
+          id: 'gid://gitlab/Issue/1',
+          state: 'closed',
+          weight: 3,
+        },
+        {
+          id: 'gid://gitlab/Issue/2',
+          state: 'opened',
+          weight: 5, // Should be ignored
+        },
+        {
+          id: 'gid://gitlab/Issue/3',
+          state: 'closed',
+          weight: 2,
+        },
+      ];
+
+      const velocity = VelocityCalculator.calculate(issues);
+
+      expect(velocity).toBe(5); // Only 3 + 2 (closed issues)
+    });
   });
 });
